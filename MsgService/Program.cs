1c882929c1a3d318669677ce8c82ce21ad62c4ca
zhixin9001 <zhixin9001@live.com>
2018-09-26 06:45:45 +0800

    init

---
+++ MsgService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MsgService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BuildWebHost(args).Run();
+            CreateWebhostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebhostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            string ip = config["ip"];
+            string port = config["port"];
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseUrls($"http://{ip}:{port}");
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ MsgService/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MsgService
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration Configuration, IApplicationLifetime appLifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+            string ip = Configuration["ip"];
+            string port = Configuration["port"];
+            string serviceName = "MsgService";
+            string serviceId = serviceName + Guid.NewGuid();
+
+            using (var consulClient = new ConsulClient(consulConfig))
+            {
+                AgentServiceRegistration asr = new AgentServiceRegistration();
+                asr.Address = ip;
+                asr.Port = Convert.ToInt32(port);
+                asr.ID = serviceId;
+                asr.Name = serviceName;
+                asr.Check = new AgentServiceCheck()
+                {
+                    DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),
+                    HTTP = $"http://{ip}:{port}/api/Health",
+                    Interval = TimeSpan.FromSeconds(10),
+                    Timeout = TimeSpan.FromSeconds(5)
+                };
+                consulClient.Agent.ServiceRegister(asr).Wait();
+            };
+
+            appLifetime.ApplicationStopped.Register(() => {
+                using (var consulClient = new ConsulClient(consulConfig))
+                {
+                    Console.WriteLine("应用退出，开始从consul注销");
+                    consulClient.Agent.ServiceDeregister(serviceId).Wait();
+                }
+            });
+        }
+
+        private void consulConfig(ConsulClientConfiguration c)
+        {
+            c.Address = new Uri("http://127.0.0.1:8500");
+            c.Datacenter = "dc1";
+        }
+    }
+}
