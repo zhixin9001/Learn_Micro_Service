4c74c7040facf37e88147394362658d78a2a17cd
zhixin9001 <zhixin9001@live.com>
2018-09-26 21:59:03 +0800

    polly

---
+++ 4_PollyTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Polly;
+using System;
+
+namespace _4_PollyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Policy policy = Policy.Handle<ArgumentException>().Fallback(()=> {
+                Console.WriteLine("出错了");
+            });
+
+            policy.Execute(()=> {
+                Console.WriteLine("开始执行");
+                throw new ArgumentException();
+              });
+            Console.ReadKey();
+        }
+    }
+}
