c561022ba0fba7ab8726822d97e06d9ad108752e
dz33 <zhixin9001@live.com>
2018-10-08 12:36:54 +0800

    ocelot

---
+++ ProductService/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Health")]
+    public class HealthController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Content("ok");
+        }
+    }
+}
