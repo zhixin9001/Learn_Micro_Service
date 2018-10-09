13b9b74ec7ffdf0088704dc5d20af3d55d8ae593
zhixin9001 <zhixin9001@live.com>
2018-10-10 06:45:09 +0800

    jwt & id4

---
+++ ID4.IdServer/Config.cs
@@ -0,0 +1,35 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ID4.IdServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            List<ApiResource> resources = new List<ApiResource>();
+            //ApiResource第一个参数是应用的名字，第二个参数是描述
+            resources.Add(new ApiResource("MsgAPI", "消息服务API"));
+            resources.Add(new ApiResource("ProductAPI", "产品API"));
+            return resources;
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            List<Client> clients = new List<Client>();
+            clients.Add(new Client
+            {
+                ClientId = "clientPC1",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {
+                    new Secret("123321".Sha256())
+                },
+                AllowedScopes = { "MsgAPI", "ProductAPI" }
+            });
+            return clients;
+        }
+    }
+}
---
+++ _9_JWTTest1/Program.cs
@@ -0,0 +1,59 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+
+namespace _9_JWTTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Encrpty-----------------");
+            var payload = new Dictionary<string, object>
+            {
+                { "UserId", 123 },
+                { "UserName", "admin" },
+                { "exp","123456"}
+            };
+            var secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+            //不要泄露
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+            var token = encoder.Encode(payload, secret);
+            Console.WriteLine(token);
+
+            Console.WriteLine("Decrpty-----------------");
+            var secret1 = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+            try
+            {
+                IJsonSerializer serializer1 = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer1, provider);
+                IBase64UrlEncoder urlEncoder1 = new JwtBase64UrlEncoder();
+                IJwtDecoder decoder = new JwtDecoder(serializer1, validator, urlEncoder1);
+                var json = decoder.Decode(token, secret1, verify: true);
+                Console.WriteLine(json);
+
+                var json1 = decoder.Decode(token);
+                Console.WriteLine(json1);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Token format invalid");
+            }
+            catch (TokenExpiredException)
+            {
+                Console.WriteLine("Token has expired");
+            }
+            catch (SignatureVerificationException)
+            {
+                Console.WriteLine("Token has invalid signature");
+            }
+            Console.ReadKey();
+        }
+    }
+}
