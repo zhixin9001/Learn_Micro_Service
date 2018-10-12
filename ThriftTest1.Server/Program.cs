e6c25212f712239a3dbfa97c76072f4f395f771c
zhixin9001 <zhixin9001@live.com>
2018-10-12 21:19:13 +0800

    thrift

---
+++ ThriftTest1.Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace ThriftTest1.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TServerTransport transport = new TServerSocket(8800);
+            var processor = new RuPeng.ThriftTest1.Contract.UserService.Processor(new
+            UserServiceImp());
+            TServer server = new TThreadPoolServer(processor, transport);
+            server.Serve();
+            Console.ReadKey();
+        }
+    }
+}
